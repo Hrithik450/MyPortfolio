b3d8966b3ab2a03f1557f3b5058ea78cf57d18a4
Hruthik <mhrithik450@gmail.com>
2025-04-26 22:51:21 +0530

    5thcommit

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -3,14 +3,27 @@ import "../globals.css";
 import Footer from "@/components/layout/footer";
 import Navbar from "@/components/layout/navbar";
 
-const metadata = {
+export const metadata = {
   title: "Hruthik M",
-  description: "Full Stack Developer",
+  description: "Full Stack Developer specializing in modern web technologies.",
+  openGraph: {
+    title: "Hruthik M",
+    description: "Welcome to Hruthik's professional portfolio site!",
+    url: "https://res.cloudinary.com/duozomapm/image/upload/v1736677246/Untitled_design__18_-removebg-preview_nnhgj4.png",
+    siteName: "Hruthik M",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/duozomapm/image/upload/v1736677246/Untitled_design__18_-removebg-preview_nnhgj4.png"
+          sizes="any"
+        />
+      </head>
       <body cz-shortcut-listen="false">
         <Navbar />
         <main>{children}</main>
